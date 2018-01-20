845b3042588c4ab6fdd2a76b139b73110a8150bc
Fatim Zohra <22501003+FatimZohra@users.noreply.github.com>
2018-01-20 22:16:30 +0000

    Add files via upload

---
+++ Exercice2.js
@@ -0,0 +1,12 @@
+
+var arr = ['shoes', 'shirts', 'socks', 'sweaters'];
+
+function calcNombre(tab){
+    if(tab){
+	    if(tab.length >50){
+		    console.log(tab.length);
+		}
+	}
+}
+
+calcNombre(arr);
